486911fbc8bb7491212d5790c1087dc8e824514b
omaisafzal <126196753+omaiss@users.noreply.github.com>
2024-05-07 20:14:18 +0500

    Source Code

--- timetable.py
+++ timetable.py
@@ -1,2 +1,33 @@
-if __name__ == '__main__':
+import random
+
+# The chromosomes should be binary encoded with the following information:
+# Course, Theory/Lab, Section, Section-Strength, Professor, First-lecture-day,
+# First- lecture-timeslot, First-lecture-room, First-lecture-room-size, Second lecture-day,
+# Second-lecture-timeslot, Second-lecture-room, Second-lecture room-size
+university_dict = {
+    "Course": 3,
+    "Theory": 3,
+    "Lab": 3,
+    "Section": 3,
+    "Strength": 3,
+    "FL_Day": 3,
+    "FLT_Slot": 3,
+    "FL_Room": 3,
+    "FLR_Size": 3,
+    "SL_Day": 3,
+    "SLT_Slot": 3,
+    "SL_Room": 3,
+    "SLR_Size": 3
+}
+
+
+def populate_chromosome():
+    pop = []
+    for index, each in enumerate(university_dict):
+        pop.append([random.randint(0, 1) for _ in range(university_dict[each])])
+    return pop
+
+
+if __name__ == "__main__":
+    print(populate_chromosome())
     pass
