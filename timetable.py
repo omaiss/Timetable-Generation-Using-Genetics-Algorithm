d48acf4c0ec45b9ff774c49cc42f71611caceda0
omaisafzal <126196753+omaiss@users.noreply.github.com>
2024-05-09 10:12:12 +0500

    Source Code

--- timetable.py
+++ timetable.py
@@ -4,30 +4,50 @@ import random
 # Course, Theory/Lab, Section, Section-Strength, Professor, First-lecture-day,
 # First- lecture-timeslot, First-lecture-room, First-lecture-room-size, Second lecture-day,
 # Second-lecture-timeslot, Second-lecture-room, Second-lecture room-size
+
+courses = ['Database', 'Programming Fundamentals', 'Data Structures', 'Algorithms', 'Computer Networks']
+theory_lab = ['Theory', 'Lab']
+sections = ['A', 'B', 'C']
+section_strength = [random.randint(30, 120) for _ in range(len(sections))]
+professors = ['Prof. Ahmed Khan', 'Prof. Fatima Ali', 'Prof. Mohammad Khan']
+lecture_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+lecture_timeslots = ['8:30 - 9:50', '10:00 - 11:20', '11:30 - 12:50', '1:00 - 2:20', '2:30 - 3:50']
+lecture_rooms = ['101', '102', '103']
+room_sizes = [60, 120]
+
 university_dict = {
-    "Course": 3,
-    "Theory": 3,
-    "Lab": 3,
-    "Section": 3,
-    "Strength": 3,
+    "Course": 5,
+    "Theory/Lab": 1,
+    "Section": 5,
+    "Strength": 5,
     "FL_Day": 3,
-    "FLT_Slot": 3,
-    "FL_Room": 3,
-    "FLR_Size": 3,
+    "FLT_Slot": 5,
+    "FL_Room": 5,
+    "FLR_Size": 1,
     "SL_Day": 3,
-    "SLT_Slot": 3,
-    "SL_Room": 3,
-    "SLR_Size": 3
+    "SLT_Slot": 5,
+    "SL_Room": 5,
+    "SLR_Size": 1
 }
 
 
 def populate_chromosome():
     pop = []
-    for index, each in enumerate(university_dict):
-        pop.append([random.randint(0, 1) for _ in range(university_dict[each])])
+    for _ in courses:
+        for index, each in enumerate(university_dict):
+            a = [random.randint(0, 1) for _ in range(university_dict[each])]
+            while a in pop:
+                a = [random.randint(0, 1) for _ in range(university_dict[each])]
+            pop.append(a)
     return pop
 
 
 if __name__ == "__main__":
-    print(populate_chromosome())
-    pass
+    pop = populate_chromosome()
+    j = 0
+    for i in range(len(courses)):
+        print('[', end="")
+        for _ in range(len(university_dict)):
+            print(pop[j], end="")
+            j += 1
+        print(']')
